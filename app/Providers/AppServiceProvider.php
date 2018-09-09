ec73ff32f1d68dcb74402a1e86d34d9443f8416c
Christopher Amanor <kriskit7@gmail.com>
2018-09-09 06:54:09 +0000

    created user and password_reset tables using migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Increasing StringLength to solve "Syntax error or access violation: 
+        //1071 Specified key was too long; max key length is 767 bytes" error
+        Schema::defaultStringLength(191);
+
     }
 
     /**
